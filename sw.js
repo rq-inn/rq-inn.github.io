b2d31eb7dfe88960ca791a2a3d8cedb75c5a9e4b
Mobu <rqinn.mov@gmail.com>
2026-03-31 12:11:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,6 +15,8 @@ const ASSETS_TO_CACHE = [
   "./images/rq-inn-com-qr.png",
   "./images/icon-192.png",
   "./images/icon-512.png",
+  "./images/favicon-32.png",
+  "./images/favicon-16.png",
   "./images/apple-touch-icon.png"
 ];
 
